a6467c3afdbb6be2d2902cb62f267dae943d596c
SivapriyaJayakumar <68330216+SivapriyaJayakumar@users.noreply.github.com>
2021-08-30 11:33:34 +0530

    push

--- notes.js
+++ notes.js
@@ -6,27 +6,41 @@ const removenotediv=document.querySelector(".removenote");
 const select=document.querySelector("#select");
 addnotebtn.addEventListener("click",addNote);
 var localstorage;
+var checkboxenable;
+var flagenable;
 var paroptdiv;
 var clrarr=['#FED7D7','#FEFCBF','#FEEBC8','#C6F6D5','#BEE3F8','#C3DAFE','#E9D8FD','#FED7E2'];
 function addNote(){
     const parentdiv=document.createElement('div');
     parentdiv.classList.add("parentdiv");
+    parentdiv.setAttribute("draggable","true");
     mainnotesdiv.appendChild(parentdiv);
-    const titleofnote= document.createElement("div");
+    const titleofnote= document.createElement("p");
     parentdiv.appendChild(titleofnote);
     titleofnote.classList.add("titleofnote");
-    const hr = document.createElement("hr");
-    hr.classList.add("divruler");
-    parentdiv.appendChild(hr);
     const contentdiv=document.createElement('div');
+    contentdiv.setAttribute('id','contentdiv');
     contentdiv.classList.add("contentdiv");
-    parentdiv.appendChild(contentdiv);
+    parentdiv.appendChild(contentdiv)
     contentdiv.contentEditable=true;
     titleofnote.contentEditable=true;
     const tooldiv=document.createElement('div');
     parentdiv.appendChild(tooldiv);
     tooldiv.classList.add("toolbox");
     tooldiv.contentEditable=false;
+    /*checkboxenable=document.createElement("input");
+    checkboxenable.setAttribute("type","checkbox");
+    checkboxenable.label="Show CB";
+    tooldiv.appendChild(checkboxenable);
+    checkboxenable.addEventListener('click',()=>{
+        if(checkboxenable.checked==true){
+            const p=document.createElement('p');
+            var c1=document.createElement("input");
+            c1.setAttribute("type","checkbox");
+            p.appendChild(c1);
+            contentdiv.appendChild(p);
+        }
+    });*/
     const removebtn=document.createElement('img');
     removebtn.classList.add('removebtn');
     removebtn.src="assets/recycle-bin.png";
@@ -36,15 +50,16 @@ function addNote(){
     });
     const stickynotebtn=document.createElement('img');
     stickynotebtn.classList.add('stickynotebtn');
-    stickynotebtn.src="assets/recycle-bin.png";
+    stickynotebtn.src="./assets/sticky-note.png";
     tooldiv.appendChild(stickynotebtn);
-    stickynotebtn.addEventListener('click',()=>{
-        mainnotesdiv.removeChild(parentdiv);
+    stickynotebtn.addEventListener('click',(e)=>{
+   
+        mainnotesdiv.removeChild(e.target.parentNode.parentNode);
         addStickyNote();
 
     });
     const changecolor=document.createElement('img');
-    changecolor.src="assets/recycle-bin.png";
+    changecolor.src="assets/colorchange.png";
     changecolor.classList.add('changecolor');
     tooldiv.appendChild(changecolor);
     changecolor.addEventListener('click',()=>{
@@ -60,17 +75,17 @@ function addNote(){
     const lab=document.createElement("label");
     lab.setAttribute("for","file");
     const labimg=document.createElement("img");
-    labimg.src="assets/fileupload.png";
+    labimg.src="./assets/upload1.png";
     labimg.classList.add("fileuploader");
     lab.appendChild(labimg);
     tooldiv.appendChild(fileuploader);
     tooldiv.appendChild(lab);
     const newmsgdiv=document.createElement("div");
-    fileuploader.addEventListener("change", ()=>{
+    fileuploader.addEventListener('change', ()=>{
         newmsgdiv.innerText="Selected File :"+fileuploader.value;
         contentdiv.appendChild(newmsgdiv);
-    })
-
+        
+    });
 }
 function addStickyNote(){
     const newnote=document.createElement('div');
